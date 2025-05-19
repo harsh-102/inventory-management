763a5b7ac72b03bc1ef1f18475d58abb7e47817d
Harshita <harshita@Harshitas-MacBook-Air.local>
2025-05-19 21:53:35 +0530

    Updated to use the railway port

--- app.py
+++ app.py
@@ -423,4 +423,5 @@ def get_products_with_suppliers():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
